027392960b3420258016de5b659adc3311a01093
Caio Nascimento <caiosenna01@gmail.com>
2023-01-10 21:31:23 -0300

    Inicio entendimento conceitos basicos
    Revisando todo o processo de programação em C#

---
+++ ProjetoCSharp/ProjetoCSharp/Fundamentos/PriPrograma.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCSharp.Fundamentos
+{
+    class PriPrograma
+    {
+        public static void Executar()
+        {
+            Console.Write("Primeiro ");
+            Console.WriteLine("Execução!");
+            Console.WriteLine("Entendimento do Write e WriteLine");
+        }
+    }
+}
---
+++ ProjetoCSharp/ProjetoCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ProjetoCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PriPrograma.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
